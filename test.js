9787cd5087df054d3a42170bf1cfe7052aa6720d
Julia Krüger <juliakbln@gmail.com>
2017-10-09 11:24:18 +0200

    test: add basic tests

---
+++ test.js
@@ -0,0 +1,48 @@
+const assert = require('assert')
+const Log = require('./index')
+
+const accountId = '123test'
+const repoSlug = 'testOrg/testRepo'
+const context = 'running-the-tests'
+const testDb = {
+  put: async (a) => {
+    assert.equal(a.accountId, accountId, 'correct accountId found')
+    assert.equal(a.repoSlug, repoSlug, 'correct repoSlug found')
+    assert.equal(a.context, context, 'correct context found')
+    assert.equal(a.type, 'info', 'correct type found')
+    assert.equal(a.message, 'pizza', 'correct message found')
+  }
+}
+
+const logger = Log({logsDb: testDb, accountId, repoSlug, context})
+logger.info('pizza')
+
+// logger is called with params
+const params = {name: 'greenkeeper', occupation: 'bot'}
+const test2Db = {
+  put: async (a) => {
+    assert.equal(a.accountId, accountId, 'correct accountId found')
+    assert.equal(a.repoSlug, repoSlug, 'correct repoSlug found')
+    assert.equal(a.context, context, 'correct context found')
+    assert.equal(a.type, 'info', 'correct type found')
+    assert.equal(a.message, 'pizza', 'correct message found')
+    assert.deepEqual(a.params, params, 'correct params found')
+  }
+}
+
+const logger2 = Log({logsDb: test2Db, accountId, repoSlug, context})
+logger2.info('pizza', params)
+
+// logger is called without a message
+const test3Db = {
+  put: async (a) => {
+    assert.equal(a.accountId, accountId, 'correct accountId found')
+    assert.equal(a.repoSlug, repoSlug, 'correct repoSlug found')
+    assert.equal(a.context, context, 'correct context found')
+    assert.equal(a.type, 'info', 'correct type found')
+    assert.equal(a.message, 'no message specified', 'correct message found')
+  }
+}
+
+const logger3 = Log({logsDb: test3Db, accountId, repoSlug, context})
+logger3.info()
